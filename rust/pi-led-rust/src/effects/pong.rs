f7c67ce0eb00e1dfebcd055caba5fad1221c8baa
Brian Green <briangreen@yahoo.com>
2024-01-09 22:01:51 -0500

    combine hw and sim with compile configs

---
+++ rust/pi-led-rust/src/effects/pong.rs
@@ -0,0 +1,3 @@
+pub fn pong() {
+    println!("Called the pong function!");
+}
--- rust/pi-led-rust/src/main.rs
+++ rust/pi-led-rust/src/main.rs
@@ -1,10 +1,13 @@
-use rs_ws281x::ChannelBuilder;
-use rs_ws281x::Controller;
-use rs_ws281x::ControllerBuilder;
-use rs_ws281x::StripType;
-use std::time::Duration;
+#[cfg(target_arch = "arm")]
+use rs_ws281x::{ChannelBuilder, Controller, ControllerBuilder, StripType} ;
+
+// use std::time::Duration;
 use std::{thread, time};
 
+mod effects;
+use crate::effects::pong::pong;
+
+#[cfg(target_arch = "arm")]
 fn draw_color_field(controller: &mut Controller, color: [u8; 4], wait_time: Duration) {
     let leds = controller.leds_mut(0);
     for led in leds {
@@ -14,6 +17,7 @@ fn draw_color_field(controller: &mut Controller, color: [u8; 4], wait_time: Dura
     thread::sleep(wait_time);
 }
 
+#[cfg(target_arch = "arm")]
 fn main() {
     // Construct a single channel controller. Note that the
     // Controller is initialized by default and is cleaned up on drop
@@ -41,4 +45,11 @@ fn main() {
         draw_color_field(&mut controller, [255, 0, 0, 0], two_secs);
         draw_color_field(&mut controller, [0, 0, 0, 0], two_secs);
     }
+}
+
+#[cfg(not(target_arch = "arm"))]
+fn main() {
+    pong();
+    thread::sleep(time::Duration::from_millis(2000));
+    println!("This is not a raspberry pi!");
 }
