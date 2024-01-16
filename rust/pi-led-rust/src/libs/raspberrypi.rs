2cebd376b174b1a15414d98c8b5d1a2e4c43f3c0
Brian Green <briangreen@yahoo.com>
2024-01-15 20:59:01 -0500

    fixes for rpi led hw

--- rust/pi-led-rust/src/libs/raspberrypi.rs
+++ rust/pi-led-rust/src/libs/raspberrypi.rs
@@ -25,24 +25,25 @@ thread_local!(static CONTROLLER: RefCell<Controller> = RefCell::new(ControllerBu
 #[cfg(target_arch = "arm")]
 pub fn render(mut image: Image<Rgba>) {
     let columns = get_config("columns");
-    let rows = get_config("rows");
-    let y_range_forward = [usize; rows];
+    let rows: usize = get_config("rows") as usize;
+    let mut y_range_forward: Vec<usize> = Vec::new();
     for y in 0..rows {
-        y_range_forward[y] = y;
+        y_range_forward.push(y);
     }
-    y_range_reverse = y_range_forward.rev();
+    let mut y_range_reverse = y_range_forward.clone();
+    y_range_reverse.reverse();
     CONTROLLER.with(|controller| {
         let mut binding = controller.borrow_mut();
         let mut leds = binding.leds_mut(0);
         let mut rev_col: bool = false;
         let mut i = 0;
         for x in 0..columns {
-            let mut y_range = y_range_forward;
+            let mut y_range = y_range_forward.clone();
             if rev_col {
-                y_range = y_range_reverse;
+                y_range = y_range_reverse.clone();
             }
             for y in y_range {
-                leds[i] = image.pixel(x, y).as_bytes();
+                leds[i] = image.pixel(x as u32, y as u32).as_bytes();
                 i += 1;
             }
             if rev_col {
